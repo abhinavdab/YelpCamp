f0329d64c3b2444be29b4126848e6ab2c1a94aba
abhinavdab <abhinav.dab@gmail.com>
2022-03-26 17:30:30 +0530

    github checking

--- seeds/cities.js
+++ seeds/cities.js
@@ -9,7 +9,7 @@ module.exports = [{
     },
     {
         city: "Los Angeles",
-        growth_from_2000_to_2013: "4.8%",
+        growth_from_2000_to_2013: "4.9%",
         latitude: 34.0522342,
         longitude: -118.2436849,
         population: "3884307",
